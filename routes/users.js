6d73e4b293e83feda9ae4f9e51ec1498c3b2ef05
Viktoriya Lavrukhina <tomenko_viktoriya.ru>
2022-08-04 07:40:53 +0300

    fix: добавление валидации

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,11 @@ const {
 
 router.get('', getUsers);
 router.get('/me', getInfo);
-router.get('/:userId', getUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().pattern(/[0-9a-fA-F]{24}$/),
+  }),
+}), getUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
